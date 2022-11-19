9d98bf10f234aa79d77523806859f0a1d4698c12
Victor Kovacs <victorkovacs@hotmail.com>
2022-11-19 16:04:44 +0100

    rename package containing app

--- polskogues-core/src/main/java/com/polskogues/app/PolskoguesCoreApplication.java
+++ polskogues-core/src/main/java/com/polskogues/app/PolskoguesCoreApplication.java
@@ -1,4 +1,4 @@
-package com.polskogues.polskoguescore;
+package com.polskogues.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- polskogues-core/src/test/java/com/polskogues/app/PolskoguesCoreApplicationTests.java
+++ polskogues-core/src/test/java/com/polskogues/app/PolskoguesCoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.polskogues.polskoguescore;
+package com.polskogues.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
