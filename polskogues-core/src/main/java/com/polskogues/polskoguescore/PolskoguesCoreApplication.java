3b9a35c8146f5673051156e5af77ac09f239be6b
Victor Kovacs <victorkovacs@hotmail.com>
2022-11-19 12:12:06 +0100

    add generated output from Spring initialzr

---
+++ polskogues-core/src/main/java/com/polskogues/polskoguescore/PolskoguesCoreApplication.java
@@ -0,0 +1,13 @@
+package com.polskogues.polskoguescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolskoguesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolskoguesCoreApplication.class, args);
+	}
+
+}
---
+++ polskogues-core/src/test/java/com/polskogues/polskoguescore/PolskoguesCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polskogues.polskoguescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolskoguesCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
