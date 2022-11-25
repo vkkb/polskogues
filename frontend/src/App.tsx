beb40fd3d8de7e8e3e9bca89f70719ef86b6572c
Victor Kovacs <victorkovacs@hotmail.com>
2022-11-25 22:11:06 +0100

    add prototype of Game component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,44 @@
-import React,{useState} from 'react';
+import {useState} from 'react';
 import Axios from 'axios'
-import logo from './logo.svg';
 import './App.css';
+
+function Game(props: any) {
+  const [answerIsRevealed, setAnswerIsRevealed] = useState(false)
+  const [answer, setAnswer] = useState("")
+
+  const handleAnswer = (event: { target: { value: any; }; }) => {
+    const value = event.target.value;
+    setAnswer(value);
+  };
+
+  return (
+  <div>
+    <h3>Translate "{props.question}"</h3>
+    <input type="text" onChange={handleAnswer}></input>
+    <div>
+      <button onClick={() => setAnswerIsRevealed(true)}>Submit</button>
+    </div>
+    <RevealAnswer answerIsRevealed = {answerIsRevealed} 
+      solution = {props.solution}
+      answer = {answer}>
+    </RevealAnswer>
+  </div>
+  );
+}
+
+function RevealAnswer(props: any){
+  if (!props.answerIsRevealed){
+    return <span></span>;
+  }
+  if (props.answer == props.solution) {
+    return (
+      <span>Congrats!</span>
+    );
+  }
+  return (
+    <span>False. Try again :)</span>
+  );
+}
 
 function App() {
 
@@ -24,6 +61,7 @@ function App() {
         {/* <div>
           <h4>{JSON.stringify(contents['_embedded']['contents'])}</h4>
         </div> */}
+        <Game question="oi" solution="czesc"/>
       </header>
     </div>
   );
